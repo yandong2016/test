b5679058cef805ace243835da11d4eec42e77cb3
Yandong Sun <yandongsun@Yandongs-MacBook-Pro.local>
2018-08-03 15:42:37 -0400

    test

--- Test.java
+++ Test.java
@@ -2,6 +2,17 @@ package test;
 
 public class Test {
 
+	private String name;
+	    private String address;
+	    private int number;
+
+	    public Employee(String name, String address, int number) {
+	      System.out.println("Constructing an Employee");
+	      this.name = name;
+	      this.address = address;
+	      this.number = number;
+		}
+
 	public static void main(String[] args) {
 		int a = 10;
 		int b = 15;
